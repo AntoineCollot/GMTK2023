0c149a7524753c5ada19bea7ff0e1c4ae1182188
AntoineCollot <antoine.collot@live.com>
2023-07-09 17:17:48 +0200

    Added end custcene

---
+++ Assets/Scripts/Enemies/EndCustcene.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndCustcene : MonoBehaviour
+{
+    public float afterBossDeathDelay = 2;
+    public GameObject heroMessage;
+    public float messageDelay = 3;
+    public Transform playerCustceneSprite;
+    public float animationDelay = 5;
+    public GameObject endMessage;
+
+    Vector2 bossDeathPos;
+    public Health boss;
+
+    private void Start()
+    {
+        bossDeathPos = boss.transform.position;
+        TriggerEnd();
+    }
+
+    public void TriggerEnd()
+    {
+        StartCoroutine(EndCutscene());
+    }
+
+    IEnumerator EndCutscene()
+    {
+        yield return new WaitForSeconds(afterBossDeathDelay);
+        heroMessage.SetActive(true);
+        yield return new WaitForSeconds(messageDelay);
+        heroMessage.SetActive(false);
+        yield return new WaitForSeconds(1);
+
+        PlayerMovement.Instance.gameObject.SetActive(false);
+        playerCustceneSprite.transform.position = bossDeathPos;
+        playerCustceneSprite.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(animationDelay);
+
+        endMessage.SetActive(true);
+    }
+}
